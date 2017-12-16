8bea393ed033036bbff1650a2b41d2563d73ae4b
Vchastinet <victorchastinet@live.com>
2017-12-15 23:34:35 -0300

    added some features

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+  imagemin = require('gulp-imagemin'),
+  csslint = require('gulp-csslint'),
+  autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('csslint', function(){
+  gulp.src('src/**/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+});
+
+gulp.task('css-prefixer', function() {
+  gulp.src('dist/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-img',  function(){
+  gulp.src('dist/assets/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('default', function(){
+  gulp.start('build-img', 'css-prefixer');
+});
+  
