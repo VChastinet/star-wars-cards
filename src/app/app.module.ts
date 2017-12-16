6b104c0b9a0c6f3cfce498b6626db181830caa57
Vchastinet <victorchastinet@live.com>
2017-12-15 21:13:00 -0300

    added the routes outlet and a header and a footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { MaterializeModule } from 'angular2-materialize';
+
+import { SwapiService } from './services/swapi.service';
+import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
-import { CardsModule } from './cards/cards.module';
+import { PageTemplateComponent } from './page-template/page-template.component';
+import { LoadingComponent } from './loading/loading.component';
+import { MainCardsComponent } from './cards/main-cards.component';
+import { FilmPageComponent } from './cards/film-cards/film-page.component';
+import { PersonPageComponent } from './cards/people-cards/person-page.component';
+import { SpeciePageComponent } from './cards/species-cards/specie-page.component';
+import { ShipPageComponent } from './cards/ships-cards/ship-page.component';
+import { VehiclePageComponent } from './cards/vehicles-cards/vehicle-page.component';
+import { PlanetPageComponent } from './cards/planets-cards/planet-page.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardsComponent,
+    FilmPageComponent,
+    PageTemplateComponent,
+    LoadingComponent,
+    PersonPageComponent,
+    SpeciePageComponent,
+    ShipPageComponent,
+    VehiclePageComponent,
+    PlanetPageComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    CardsModule
+    CommonModule,
+    HttpClientModule,
+    MaterializeModule,
+    routing
   ],
-  providers: [],
+  providers: [SwapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
