be8221559623500cb32f0603cf4c14ea54492b52
Vchastinet <victorchastinet@live.com>
2017-12-15 21:17:02 -0300

    page for each item of the species category

---
+++ src/app/cards/species-cards/specie-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpeciePageComponent } from './specie-page.component';
+
+describe('SpeciePageComponent', () => {
+  let component: SpeciePageComponent;
+  let fixture: ComponentFixture<SpeciePageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpeciePageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpeciePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
